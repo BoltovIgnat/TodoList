e75e1139cccd685f3c69a3ba4660d5e055eddb4d
BoltovIgnat <uncelpeter@gmail.com>
2017-09-13 09:45:27 +0400

    first commit

---
+++ app/shared/data.ts
@@ -0,0 +1,14 @@
+export const todos = [
+    {
+        title: 'Сделать 1 задачу',
+        completed: true
+    },
+    {
+        title: 'Сделать 2 задачу',
+        completed: false
+    },
+    {
+        title: 'Сделать 3 задачу',
+        completed: false
+    },
+];
